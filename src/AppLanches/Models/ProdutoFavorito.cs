1ebd470333c2b2d3eca585fa0202c12ed70da920
PauloAlves8039 <pj38alves@gmail.com>
2024-10-21 15:44:41 -0300

    feat: criada classe e serviço para favoritos

---
+++ src/AppLanches/Models/ProdutoFavorito.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace AppLanches.Models;
+
+public class ProdutoFavorito
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Detalhe { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public bool IsFavorito { get; set; }
+}
